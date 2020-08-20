75b50cb45372629a275b75e5895e46fcb75fcf53
Kyle Gardner <kb_gardner@icloud.com>
2020-08-20 15:20:17 -0600

    Initial Commit

---
+++ The Merchant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Merchant
+//
+//  Created by Kyle Gardner on 8/20/20.
+//  Copyright © 2020 Kyle Gardner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ The MerchantTests/The_MerchantTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_MerchantTests.swift
+//  The MerchantTests
+//
+//  Created by Kyle Gardner on 8/20/20.
+//  Copyright © 2020 Kyle Gardner. All rights reserved.
+//
+
+import XCTest
+@testable import The_Merchant
+
+class The_MerchantTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
